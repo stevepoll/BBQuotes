b842a567476d219c99d02ddf27a16acb8b5318c0
Steve Poll <stevepoll@hotmail.com>
2024-01-22 16:30:12 -0500

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Esteban Poll on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
