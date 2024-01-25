0fddfe66a8587161f6a9f1a43e78e2a558ae1cf7
Steve Poll <stevepoll@hotmail.com>
2024-01-24 22:55:55 -0500

    Working on character view

---
+++ BBQuotes/CharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterView.swift
+//  BBQuotes
+//
+//  Created by Esteban Poll on 1/24/24.
+//
+
+import SwiftUI
+
+struct CharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterView()
+}
